dd827e1d467cd6e39de5fe4f0f66c1f44c60d112
azkool <87613755+azkool@users.noreply.github.com>
2024-07-01 23:25:27 +0100

    ...

--- nav.js
+++ nav.js
@@ -2,8 +2,10 @@ const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
-  const visibility = primaryNav.getAttribute("data-visible");
-
-  
-  
+  primaryNav.classList.toggle("open");
+  navToggle.classList.toggle("open");
+  //   console.log(primaryNav.classList.contains("open"));
 });
+
+//   console.log(visibility);
+  
