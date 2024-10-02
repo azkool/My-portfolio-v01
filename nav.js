39c314d46bf4899897c8fc08047185a70c67dadd
azkool <87613755+azkool@users.noreply.github.com>
2024-10-02 17:14:22 +0100

    Need to review this code

--- nav.js
+++ nav.js
@@ -1,9 +1,8 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener("click", () => {
+const abs = navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute('data-visible');
-
     //console.log(visiblity)
     if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
@@ -14,13 +13,26 @@ navToggle.addEventListener("click", () => {
     }
 });
 
+// Get the navigation bar and the links inside it
+const navBar = document.querySelector('.primary-navigation');
+const links = navBar.querySelectorAll('.menu-link');
+
+// Add a click event listener to each link
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    // Toggle the visibility of the navigation bar
+    navBar.classList.toggle('collapsed');
+    // Close the navigation bar when a link is clicked
+    navToggle.setAttribute("aria-expanded", false);
+    
+  });
+});
+
 // Time of day greeting
 const greeting = document.querySelector(".time-of-day");
-
     document.addEventListener("DOMContentLoaded", () => {
         const currentTime = new Date();
         const hours = currentTime.getHours();
-        console.log(hours);
         if (hours >= 0 && hours < 12) {
             greeting.textContent = "morning!";
         } else if (hours >= 12 && hours < 18) {
@@ -28,6 +40,5 @@ const greeting = document.querySelector(".time-of-day");
         } else if (hours >= 18 || hours < 24) {
             greeting.textContent = "evening!";
         }
-        
     });
 
